ff3ddbb5398513d04f3dedd3f0f4f90ff2cdc96d
Andrew Maia <andrew.maia@alperseguros.com.br>
2024-11-14 20:24:14 -0300

    first commit

---
+++ Oauth2/Client/Controllers/ContatoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Text.Json;
+using Microsoft.Identity.Client;
+
+namespace MvcClient2.Controllers;
+
+public class ContatoController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public ContatoController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+
+    }
+    public IActionResult Index()
+    {
+        var query = new QueryBuilder();
+        query.Add("client_id", "");
+        query.Add("scope", "api://xxxxxx/contato");
+        query.Add("response_type", "code");
+        query.Add("redirect_uri", "https://localhost:7080/contato/ContatoCallback");
+        string url = $"https://login.microsoftonline.com/xxxxxxx/oauth2/v2.0/authorize{query.ToQueryString()}";
+        return Redirect(url);
+    }
+
+
+    public async Task<IActionResult> ContatoCallback(string code)
+    {
+        var app = ConfidentialClientApplicationBuilder.Create("xxxx")
+               .WithClientSecret("xxxx")
+               .WithRedirectUri("https://localhost:7080/contato/ContatoCallback")
+               .WithAuthority(new Uri("https://login.microsoftonline.com/7cd172cf-0ad7-4cef-b74f-b3866d4aa291"))
+               .Build();
+
+        var result = await app.AcquireTokenByAuthorizationCode(new[] { "api://xxxxxx/contato" }, code).ExecuteAsync();
+
+        var contatoClient = new HttpClient();
+        contatoClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        var response = await contatoClient.GetAsync("https://localhost:6001/contato");
+        string retorno = await response.Content.ReadAsStringAsync();
+        var contatos = JsonSerializer.Deserialize<List<Contato>>(retorno);
+        return View("Index", contatos);
+    }
+}
+
+public class Contato
+{
+    public string nome { get; set; }
+    public string telefone { get; set; }
+
+}
---
+++ Oauth2/ResourceServer/CheckClaimMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public class CheckClaimMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly string _claimType;
+    private readonly string _claimValue;
+
+    public CheckClaimMiddleware(RequestDelegate next, string claimType, string claimValue)
+    {
+        _next = next;
+        _claimType = claimType;
+        _claimValue = claimValue;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var user = context.User;
+        if (user.HasClaim(c => c.Type == _claimType && c.Value == _claimValue))
+            Console.WriteLine($"Possui o claim {_claimType}:{_claimValue} ");
+        else
+            Console.WriteLine($"Não possui o claim {_claimType}:{_claimValue} ");
+
+
+        await _next(context);
+    }
+}
